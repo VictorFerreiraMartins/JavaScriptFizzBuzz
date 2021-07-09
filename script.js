bfbc58db65f5f8d3a2b19c3f1364ca0a8197ee27
VictorFerreiraMartins <victorfm@live.com>
2021-07-09 19:24:26 -0300

    Exercício Final - ToDo List

--- script.js
+++ script.js
@@ -1,47 +1,35 @@
-// Exercício 1 - FizzBuzz
-// Divisivel por 3 => 'Fizz',
-// Divisivel por 5 => 'Buzz',
-// Divisivel por 3 e 5 => 'FizzBuzz',
-// Se não for um número => 'Não é um número'
-// Se não for divisível nem por 3 e nem por 5 => Entrada
-/*
-let resultado = fizzBuzz(4);
-console.log(resultado)
+const listContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
 
-function fizzBuzz(entrada){
-    if (typeof entrada !== 'number')
-        return 'Não é um número';
-    if ((entrada % 3 === 0) && (entrada % 5 === 0))
-        return 'FizzBuzz';
-    if (entrada % 3 === 0)
-        return 'Fizz';
-    if (entrada % 5 === 0)
-        return 'Buzz';
-    return entrada;
-}
-*/
+let lists = []
 
-// Exercício 2 - Reverse a string
-/*
-let newStr ='';
+newListForm.addEventListener('submit',function(e){
+    e.preventDefault()
+    const listName = newListInput.value
+    if (listName === null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    render()
+})
 
-function reverseAString(str){
-    for (let i = str.length - 1; i >=0; i--){
-    newStr += str[i];
-    console.log(newStr);
-    }
+function createList(name){
+    return { id: Date.now().toString(), name: name}
 }
 
-let resultado = reverseAString('Victor')
-*/
-
-// Exercício 3 - Converter Celsius para Fahrenheit
-
-function convertToFahrenheit(value){
-    return (value * 1.8) + 32
+function render(){
+    clearElement(listContainer)
+    lists.forEach(function(list) {
+        const item = document.createElement('li')
+        item.classList.add('item')
+        item.innerText = list.name
+        listContainer.appendChild(item)
+    })
 }
 
-let result = convertToFahrenheit(0)
-
-console.log(`O valor em Farhrenheit é ${result}°`)
-
+function clearElement(element){
+    while(element.firstChild)
+        element.removeChild(element.firstChild)
+}
+render();
