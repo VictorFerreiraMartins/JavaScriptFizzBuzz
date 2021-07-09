072383cf4629dd88ca6e6a61de3f7b6c6bc0337f
VictorFerreiraMartins <victorfm@live.com>
2021-07-09 17:56:20 -0300

    Atualizando arquivo script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-class Person{
-    constructor(name){
-        this._name = name;
-    }
-    get name(){
-        return this._name
-    }
+// FizzBuzz
+// Divisivel por 3 => 'Fizz',
+// Divisivel por 5 => 'Buzz',
+// Divisivel por 3 e 5 => 'FizzBuzz',
+// Se não for um número => 'Não é um número'
+// Se não for divisível nem por 3 e nem por 5 => Entrada
 
-    set name(value){
-        this._name = value;
-    }
+let resultado = fizzBuzz(4);
+console.log(resultado)
+
+function fizzBuzz(entrada){
+    if (typeof entrada !== 'number')
+        return 'Não é um número';
+    if ((entrada % 3 === 0) && (entrada % 5 === 0))
+        return 'FizzBuzz';
+    if (entrada % 3 === 0)
+        return 'Fizz';
+    if (entrada % 5 === 0)
+        return 'Buzz';
+    return entrada;
 }
-
-let person = new Person("Victor");
-
-console.log(person.name)
-
-person.name = 'Amanda'
-
-console.log(person.name)
